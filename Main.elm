b2b98db8a6776bbf49d8b83b82a4f4d48c4fb5af
Calvyn82 <claytonflesher@gmail.com>
2016-10-15 20:27:39 -0500

    Working proof of concept. Added list correct answers Added list of scores

--- Main.elm
+++ Main.elm
@@ -1,4 +1,4 @@
-import Html exposing (Attribute, div, text, input)
+import Html exposing (Attribute, div, text, input, ul, li, br, span)
 import Html.App as Html
 import Html.Attributes as H exposing (..)
 import Html.Events exposing (on, onInput)
@@ -15,15 +15,18 @@ update (Update v) model =
     String.toInt (Debug.log "" v) |> Result.withDefault 0
 
 view model =
-  div []
-    [ input
-      [ type' "range"
-      , H.min "0"
-      , H.max "100"
-      , value <| toString model
-      , onInput Update
-      ] []
-    , text <| toString model
+  div [class "container"] [
+      span [] [text "Number of Questions"]
+    , br [] []
+    , input [ type' "range", H.min "0", H.max "100", value <| toString model, onInput Update] []
+    , br [] []
+    , span [] [text <| toString model]
+    , br [] []
+    , span [] [text "Correct"]
+    , renderCorrectList model
+    , span [] [text "Score"]
+    , br [] []
+    , renderScoreList model
     ]
 
 main =
@@ -32,3 +35,23 @@ main =
     , view = view
     , update = update
     }
+
+renderCorrectList : Int -> Html.Html Msg
+renderCorrectList n =
+  [0..n]
+  |> List.map (\x -> li [] [ text (toString x) ])
+  |> List.reverse
+  |> ul [class "list-group"]
+
+
+renderScoreList : Int -> Html.Html Msg
+renderScoreList n =
+  [0..n]
+  |> List.map (\x -> li [] [ text (calculateScore n x) ])
+  |> List.reverse
+  |> ul [class "list-group"]
+
+calculateScore : Int -> Int -> String
+calculateScore total correct =
+  round (((toFloat correct) / (toFloat total)) * 100)
+  |> toString
